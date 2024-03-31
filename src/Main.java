e87ae377fc54dadcab1e3991e745c8462002240a
Musin23 <164151718+Musin23@users.noreply.github.com>
2024-03-31 19:47:29 +0500

    Alternative test task

---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+//Исправил и релизовал пункт 7.
+public class Main {
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return strNum.matches("-?\\d+(\\.\\d+)?");
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+        char action;
+        String[] data;
+        if (exp.contains(" + ")) {
+            data = exp.split(" \\+ ");
+            action = '+';
+        } else if (exp.contains(" - ")) {
+            data = exp.split(" - ");
+            action = '-';
+        } else if (exp.contains(" * ")) {
+            data = exp.split(" \\* ");
+            action = '*';
+        } else if (exp.contains(" / ")) {
+            data = exp.split(" / ");
+            action = '/';
+        }else{
+            throw new Exception("Некорректный знак действия");
+        }
+        if (action == '*' || action == '/') {
+            if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число");
+        }
+        for (int i = 0; i < data.length; i++) {
+            if (isNumeric( data[i]) && Integer.valueOf(data[i])>10) {
+                throw new Exception("Введено число больше 10 ");
+            }
+            if (isNumeric( data[i]) && Integer.valueOf(data[i])<0) {
+                throw new Exception("Введено число меньше 0 ");
+            }
+            data[i] = data[i].replace("\"", "");
+        }
+
+        if (action == '+') {
+            printInQuotes(data[0] + data[1]);
+        } else if (action == '*') {
+            int multiplier = Integer.parseInt(data[1]);
+            String result = "";
+            for (int i = 0; i < multiplier; i++) {
+                result+=data[0];
+            }
+            printInQuotes(result);
+        } else if (action == '-') {
+            int index = data[0].indexOf(data[1]);
+            if(index == -1){
+                printInQuotes(data[0]);
+            }else{
+                String result = data[0].substring(0, index);
+                result+=data[0].substring(index+data[1].length());
+                printInQuotes(result);
+            }
+        }else{
+            int newLen = data[0].length()/Integer.parseInt(data[1]);
+            String result = data[0].substring(0,newLen);
+            printInQuotes(result);
+        }
+        if (data[0].length()>10)
+            throw new Exception("Введено больше 10 символов");
+        if (data[1].length()>10)
+            throw new Exception("Введено больше 10 символов");
+    }
+    static void printInQuotes(String text){
+        System.out.println("\""+text+"\"");
+        if (text.length()>40)
+            System.out.println(""+text.substring(0,40)+"..."+"");
+    }
+}
